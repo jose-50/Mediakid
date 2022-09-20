1c2b1cdaf923955a5bcc1fd77210e781b873e62b
mariela <bs21002ts@esfe.agape.edu.sv>
2022-09-20 10:26:13 -0600

    videotest

--- MediaKid.AccesoADatos/VideoDAL.cs
+++ MediaKid.AccesoADatos/VideoDAL.cs
@@ -130,3 +130,4 @@ namespace MediaKid.AccesoADatos
     }
     
 
+h
--- MediaKid.PruebasUnitarias/VideoDALTests.cs
+++ MediaKid.PruebasUnitarias/VideoDALTests.cs
@@ -1,56 +1,101 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MediaKid.AccesoADatos;
+using MediaKid.EntidadesDeNegocio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MediaKid.AccesoADatos.Tests
+namespace DeptosES.AccesoADatos.Tests
 {
     [TestClass()]
     public class VideoDALTests
     {
+        private static Video videoInicial = new Video { Id = 2, IdCategoria = 1 };
+
         [TestMethod()]
-        public void CrearAsyncTest()
+        public async Task T1CrearAsyncTest()
         {
-            Assert.Fail();
+            var video = new Video();
+            video.IdCategoria = videoInicial.IdCategoria;
+            video.Nombre = "vaca lola";
+            video.Url = "urrl de video";
+            video.Descripcion = "videos infantiles de la vaca  lola";
+            video.Duracion = "3:00";
+            int result = await VideoDAL.CrearAsync(video);
+            Assert.AreNotEqual(0, result);
+            videoInicial.Id = video.Id;
         }
 
         [TestMethod()]
-        public void ModificarAsyncTest()
+        public async Task T2ModificarAsyncTest()
         {
-            Assert.Fail();
+            var video = new Video();
+             video.IdCategoria = videoInicial.IdCategoria;
+            video.Nombre = "la vaca lola";
+            video.Url = "url del video";
+            video.Descripcion = "Municipio famoso por el puerto que lleva su mismo nombre";
+            video.Duracion= "3:00";
+            int result = await VideoDAL.ModificarAsync(video);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void EliminarAsyncTest()
+        public async Task T3ObtenerPorIdAsyncTest()
         {
-            Assert.Fail();
+            var video = new Video();
+            video.Id = videoInicial.Id;
+            var resultVideo = await VideoDAL.ObtenerPorIdAsync(video);
+            Assert.AreEqual(video.Id, resultVideo.Id);
         }
 
         [TestMethod()]
-        public void ObtenerPorIdAsyncTest()
+        public async Task T4ObtenerTodosAsyncTest()
         {
-            Assert.Fail();
+            var resultVideos = await VideoDAL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultVideos.Count);
         }
 
         [TestMethod()]
-        public void ObtenerTodosAsyncTest()
+        public async Task T5BuscarAsyncTest()
         {
-            Assert.Fail();
+            var video= new Video();
+            video.IdCategoria = videoInicial.IdCategoria;
+            video.Nombre = "l";
+            video.Url = "u";
+            video.Descripcion = "a";
+            video.Duracion = "3:00";
+            video.Top_Aux = 10;
+            var resultVideos = await VideoDAL.BuscarAsync(video);
+            Assert.AreNotEqual(0, resultVideos.Count);
         }
 
         [TestMethod()]
-        public void BuscarAsyncTest()
+        public async Task T6BuscarIncluirCategoriasAsyncTest()
         {
-            Assert.Fail();
+            var video = new Video();
+            video.IdCategoria= videoInicial.IdCategoria;
+            video.Nombre = "l";
+            video.Url = "u";
+            video.Descripcion = "a";
+            video.Duracion = "3:00";
+            video.Top_Aux = 10;
+            var resultVideos = await VideoDAL.BuscarIncluirCategoriasAsync(video);
+            Assert.AreNotEqual(0, resultVideos.Count);
+            var ultimoVideo = resultVideos.FirstOrDefault();
+            Assert.IsTrue(ultimoVideo.Categoria != null && video.IdCategoria == ultimoVideo.Categoria.Id);
         }
 
         [TestMethod()]
-        public void BuscarIncluirCategoriasAsyncTest()
+        public async Task T7EliminarAsyncTest()
         {
-            Assert.Fail();
+            var video = new Video();
+            video.Id =videoInicial.Id;
+            int result = await VideoDAL.EliminarAsync(video);
+            Assert.AreNotEqual(0, result);
         }
     }
 }
